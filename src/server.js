e721e7d4f573c06999729d96cb51b657f11fb862
nnourr <nourqwerty123@gmail.com>
2024-12-20 14:36:09 -0500

    no auth pls

--- src/server.js
+++ src/server.js
@@ -1,15 +1,8 @@
 import * as uws from 'uws'
-import * as env from 'lib0/environment'
 import * as logging from 'lib0/logging'
 import * as error from 'lib0/error'
-import * as jwt from 'lib0/crypto/jwt'
-import * as ecdsa from 'lib0/crypto/ecdsa'
-import * as json from 'lib0/json'
 import { registerYWebsocketServer } from '../src/ws.js'
 import * as promise from 'lib0/promise'
-
-const wsServerPublicKey = await ecdsa.importKeyJwk(json.parse(env.ensureConf('auth-public-key')))
-// const wsServerPrivateKey = await ecdsa.importKeyJwk(json.parse(env.ensureConf('auth-private-key')))
 
 class YWebsocketServer {
   /**
@@ -46,24 +39,7 @@ export const createYWebsocketServer = async ({
   const app = uws.App({})
   await registerYWebsocketServer(app, '/:room', store, async (req) => {
     const room = req.getParameter(0)
-    const headerWsProtocol = req.getHeader('sec-websocket-protocol')
-    const [, , token] = /(^|,)yauth-(((?!,).)*)/.exec(headerWsProtocol) ?? [null, null, req.getQuery('yauth')]
-    if (token == null) {
-      throw new Error('Missing Token')
-    }
-    // verify that the user has a valid token
-    const { payload: userToken } = await jwt.verifyJwt(wsServerPublicKey, token)
-    if (userToken.yuserid == null) {
-      throw new Error('Missing userid in user token!')
-    }
-    const permUrl = new URL(`${room}/${userToken.yuserid}`, checkPermCallbackUrl)
-    try {
-      const perm = await fetch(permUrl).then(req => req.json())
-      return { hasWriteAccess: perm.yaccess === 'rw', room, userid: perm.yuserid || '' }
-    } catch (e) {
-      console.error('Failed to pull permissions from', { permUrl })
-      throw e
-    }
+    return {hasWriteAccess: true, room, userid: ""}
   }, { redisPrefix, initDocCallback })
 
   await promise.create((resolve, reject) => {
